365a4bcd959c411dcb066ffe79c774b663e086e2
Kurt De Jonghe <kurt.de.jonghe2@gmail.com>
2022-08-01 11:14:28 +0200

    fixed problem with updating spending per shop by JDJ and KDJ

--- Frames/output_frame.py
+++ Frames/output_frame.py
@@ -4,19 +4,16 @@ from datetime import date
 
 import tkmacosx as tkm
 
-
 class OutputFrame:    
     def __init__(self) -> None:
         # connecting to db en creating a cursor
         self.connect = sq3.connect("./Data/acounting.db")
         self.cursor = self.connect.cursor()
 
-        ## GUI SECTION
-        # output variabels
+        ## GUI
+        # variabels
         self.curr_date = date.today().strftime("%d-%m-%Y")
-        self.curr_delhaize_spending = self.total_delhaize()
-        self.curr_match_spending = self.total_match()
-        self.curr_total_spending = self.total_spending()
+        # self.curr_total_spending = self.total_spending_all_shops()
 
         # frames
         self.output_fr = tk.LabelFrame(text="Overzicht uitgaven")
@@ -26,18 +23,12 @@ class OutputFrame:
         self.current_date_lbl = tk.Label(
             self.output_fr, text=f"Huidige datum: {self.curr_date}"
         )
-        self.total_shops_spending_lbl = tk.Label(
-            self.output_fr, text="Testing kurt"
+        self.total_per_shops_spending_lbl = tk.Label(
+            self.output_fr, text=f""
         )
-        # self.total_delhaize_lbl = tk.Label(
-        #     self.output_fr,
-        #     text=f"Maand uitgave Delhaize: €{self.curr_delhaize_spending}",
-        # )
-        # self.total_match_lbl = tk.Label(
-        #     self.output_fr, text=f"Maand uitgave Match: €{self.curr_match_spending}"
-        # )
-        self.total_spend_lbl = tk.Label(
-            self.output_fr, text=f"Totaal maand uitgave: €{self. curr_total_spending}"
+
+        self.total_spend_all_shops_lbl = tk.Label(
+            self.output_fr, text=f"Totaal maand uitgave: €{self.total_spending_all_shops()}"
         )
 
         # buttons
@@ -48,16 +39,13 @@ class OutputFrame:
             fg="white",
             width=265,
             overbackground="green",
-            # command=self.update_labels,
-            command=self.total_per_shop,
+            command=self.total_spending_per_shop,
         )
 
         # grid position for labels and buttons
-        self.current_date_lbl.grid(row=0, column=0)
-        self.total_shops_spending_lbl.grid(row=1, column=0)
-        # self.total_delhaize_lbl.grid(row=1, column=0)
-        # self.total_match_lbl.grid(row=2, column=0)
-        self.total_spend_lbl.grid(row=3, column=0)
+        self.current_date_lbl.grid(row=0, column=0, columnspan=5)
+        self.total_per_shops_spending_lbl.grid(row=1, column=0, columnspan=5)
+        self.total_spend_all_shops_lbl.grid(row=3, column=0, columnspan=5)
         self.update_btn.grid(
             row=4, column=0, columnspan=5, sticky="nsew", padx=20, pady=10
         )
@@ -66,77 +54,44 @@ class OutputFrame:
         for widget in self.output_fr.winfo_children():
             widget.grid_configure(padx=10, pady=5, sticky="nsew")
 
-    ## FUNCTIONS SECTION
-    def total_delhaize(self):
-        self.cursor.execute(
-            "SELECT sum (Bedrag) FROM uitgaven WHERE (Winkel) IS 'Delhaize';"
-        )
-        self.connect.commit()
-        self.rows = self.cursor.fetchall()
-        self.row = self.rows[0]
-        for self.num in self.row:
-            round(self.num, 2)
-        return self.num
-
-    def total_match(self):
-        self.cursor.execute(
-            "SELECT sum (Bedrag) FROM uitgaven WHERE (Winkel) IS 'Match';"
-        )
-        self.connect.commit()
-        self.rows = self.cursor.fetchall()
-        self.row = self.rows[0]
-        for self.num in self.row:
-            round(self.num, 2)
-        return self.num
-
-    def total_per_shop(self):
+    ## FUNCTIONS
+    def total_spending_per_shop(self):
         # fetching shops out of db
         shops_list = self.cursor.execute("SELECT Winkel FROM uitgaven;")
-
+                
         # filter out duplicate shops
-        f_shop = []
+        filterd_shop = []
+        result_string = ""
+        
         for shop in shops_list:
             for i in shop:
-                if i not in f_shop:
-                    f_shop.append(i)
-        for s in f_shop:
-            # print(s[::1])
-            print(s)
+                if i not in filterd_shop:
+                    filterd_shop.append(i)
 
-            # self.total_shops_spending_lbl.config(text=f"{f_shop.sort()}"
-            # )
-            self.total_shops_spending_lbl.config(text=f"{s}"
-            )
-
-        # adding filterd shops and amounts in to ther list
-        for i in f_shop:
-            self.cursor.execute("SELECT sum (Bedrag) FROM uitgaven WHERE (winkel) = ?", (i,))
-            self.rows = self.cursor.fetchall()
-            self.row = self.rows[0]
-            for r in self.row:
-                print(r)
+        # finding amount for each shop
+        for shop in sorted(filterd_shop):
+            self.cursor.execute("SELECT sum (Bedrag) FROM uitgaven WHERE (winkel) = ?", (shop,))
+            self.amounts = self.cursor.fetchall()
             
-            self.total_shops_spending_lbl.config(text=f"{f_shop}"
+            # total amount for each shop
+            for amount in self.amounts[0]:
+                result_string += f"{shop}: {amount}€\n"
+                
+        # updating shops spending label
+        self.total_per_shops_spending_lbl.config(text=f"{result_string}"
             )
+        # updating total all shops speding label
+        self.total_spend_all_shops_lbl.config(
+                text=f"Totaal maand uitgave: €{self.total_spending_all_shops()}"
+                )
 
-        self.connect.commit()
+    def total_spending_all_shops(self):
+        self.cursor.execute("SELECT sum (Bedrag) FROM uitgaven")
+        self.amount = self.cursor.fetchall()
 
-
-    def total_spending(self):
-        self.spend_1 = float(self.total_delhaize())
-        self.spend_2 = float(self.total_match())
-        total = self.spend_1 + self.spend_2
-        return round(total, 2)
-
-    def update_labels(self):
-        self.total_delhaize_lbl.config(
-            text=f"Maand uitgave Delhaize: €{self.total_delhaize()}"
-        )
-        self.total_match_lbl.config(text=f"Maand uitgave Match: €{self.total_match()}")
-        self.total_spend_lbl.config(
-            text=f"Totaal maand uitgave: €{self.total_spending()}"
-        )
-
+        for amount in self.amount[0]:
+            amount = round(amount, 2)
+            return round(amount, 2)
 
 if __name__ == "__main__":
     OutputFrame()
